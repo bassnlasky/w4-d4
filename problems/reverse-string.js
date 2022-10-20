6118adcf4f4e3f33960b778775eee92d38991795
Marcus Kim <kim.marcus97@gmail.com>
2022-10-20 13:58:35 -0700

    finished reverse-string

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,13 @@
-module.exports = function reverseString(string) {
-  // Your code here
+function reverseString(string) {
+  if (typeof string === 'string') {
+    let result = '';
+  for (let i = string.length -1; i >= 0; i--) {
+    result += string[i];
+  }
+  return result;
+  } else {
+    throw new TypeError;
+  }
 };
+
+module.exports = reverseString;
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,14 @@
-// Your code here
+const { expect } = require('chai');
+const chai = require('chai');
+const expects = chai.expect;
+const reverseString = require('../problems/reverse-string');
+
+describe('reverseString', () => {
+  it('should return the string reversed', () => {
+    expect(reverseString('fun')).to.equal('nuf');
+  })
+
+  it('should return an error if not a string', () => {
+    expect(() => reverseString(1)).to.throw(TypeError);
+  })
+})
